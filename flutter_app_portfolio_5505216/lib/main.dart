8f8a805563288faca7c143b161f3a8165a92d02e
j0rdnMotuo <jamel.motuo@gmail.com>
2024-06-14 09:10:45 +0200

    update main.dart

--- flutter_app_portfolio_5505216/lib/main.dart
+++ flutter_app_portfolio_5505216/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         brightness: Brightness.light,
       ),
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
